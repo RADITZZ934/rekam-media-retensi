06b85033008527e465bfbbb7b117aaf720e829d8
RADITYAA_PC <fridaanwarfahmi@gmail.com>
2026-07-15 14:01:40 +0700

    delete delete button

--- resources/js/pages/DataRetensi.vue
+++ resources/js/pages/DataRetensi.vue
@@ -149,16 +149,6 @@
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
                   </svg>
                 </button>
-                <!-- Delete -->
-                <button
-                  @click="tapMusnah(retensi)"
-                  class="p-2 bg-red-600 text-white rounded hover:bg-red-700 transition-colors"
-                  title="Delete"
-                >
-                  <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142a2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                  </svg>
-                </button>
               </div>
             </td>
           </tr>
